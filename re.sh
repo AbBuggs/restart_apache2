09bcac9a510edab4cfe3dc54cbb84cdbedae784c
Akil Buggs <akilbuggs@gmail.com>
2021-05-16 20:18:29 -0500

    Version 0.3.0

--- re.sh
+++ re.sh
@@ -34,3 +34,12 @@ else
     echo "ERROR: $COMMAND is an invalid service command {restart|reload}"
     exit 1
 fi
+
+
+# List all of the configuration files in the _/etc/apache2/sites-available/_ directory
+VHOSTS_PATH=/etc/apache2/sites-available/*.conf
+
+for FILENAME in $VHOSTS_PATH
+do
+  echo $FILENAME
+done
